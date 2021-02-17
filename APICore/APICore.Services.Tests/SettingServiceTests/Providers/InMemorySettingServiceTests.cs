d42126d450218f689ee3fcac5192d75b5e0a48c3
Juan Manuel Bermúdez Cabrera <jbermudezcabrera@gmail.com>
2021-02-16 22:57:13 -0500

    test: add EF in memory database provider for running tests

---
+++ APICore/APICore.Services.Tests/SettingServiceTests/Providers/InMemorySettingServiceTests.cs
@@ -0,0 +1,14 @@
+﻿using APICore.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace APICore.Services.Tests.SettingServiceTests.Providers
+{
+    public class InMemorySettingServiceTests : SettingServiceTests
+    {
+        public InMemorySettingServiceTests() : base(new DbContextOptionsBuilder<CoreDbContext>()
+                                                   .UseInMemoryDatabase("TestDatabase")
+                                                   .Options)
+        {
+        }
+    }
+}
