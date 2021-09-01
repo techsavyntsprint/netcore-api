82dfddee61dabea46160cb40a2351fbce76ed162
Pavel <pavel.mena@ntsprint.com>
2021-09-01 13:06:31 -0400

    Refactoring get logged user id using an extension.

---
+++ APICore/APICore.API/Utils/HttpContextExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace APICore.API.Utils
+{
+    public static class HttpContextExtension
+    {
+        /// <summary>
+        /// Get current user id from claim principal
+        /// </summary>
+        /// <param name="httpContextAccessor"></param>
+        /// <returns></returns>
+        public static int CurrentUser(this IHttpContextAccessor httpContextAccessor)
+        {
+            var id = httpContextAccessor?.HttpContext?.User?.GetUserIdFromToken();
+
+            return id ?? 0;
+        }
+    }
+}
